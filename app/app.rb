87a21266effd6166600402f972fa4081c5651ee4
Elin <elinnet@gmail.com>
2015-06-30 18:07:28 +0100

    added intial redirection to /links page on app.rb file, added css styling

--- app/app.rb
+++ app/app.rb
@@ -7,6 +7,11 @@ require './data_mapper_setup'
 class BookmarkManager < Sinatra::Base
   set :views, proc { File.join(root, '..', 'views') }
 
+  get '/' do
+  redirect to('/links')
+
+  end
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
