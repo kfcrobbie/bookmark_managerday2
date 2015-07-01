7e80615a52797d50a8bd5f7929c769e57a2cffcd
Robert Morgan <robbie.morgan@gmail.com>
2015-07-01 13:31:13 +0100

    Added ability for multiple tags

--- app/app.rb
+++ app/app.rb
@@ -8,7 +8,7 @@ class BookmarkManager < Sinatra::Base
   set :views, proc { File.join(root, '..', 'views') }
 
   get '/' do
-  redirect to('/links')
+    redirect to('/links')
 
   end
 
@@ -21,13 +21,20 @@ class BookmarkManager < Sinatra::Base
     erb :'links/new'
   end
 
-  post '/links' do 
-    link = Link.new(url: params[:url],     # 1. Create a link
-                title: params[:title])
-  tag  = Tag.create(name: params[:tag]) # 2. Create a tag for the link
-  link.tags << tag                       # 3. Adding the tag to the link's DataMapper collection.
-  link.save 
+  post '/links' do
+    link = Link.create(url: params[:url], title: params[:title])
+    tags = params[:tag].split(' ')
+    tags.each do |tag|
+      link.tags << Tag.create(name: tag)
+    end                      # 3. Adding the tag to the link's DataMapper collection.
+    link.save
     redirect to ('/links')
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
   end
 
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 
 feature 'Viewing links' do
 
+  before(:each) do
+    Link.create(url: 'http://www.makersacademy.com',
+                title: 'Makers Academy',
+                tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'http://www.google.com',
+                title: 'Google',
+                tags: [Tag.first_or_create(name: 'search')])
+    Link.create(url: 'http://www.zombo.com',
+                title: 'This is Zombocom',
+                tags: [Tag.first_or_create(name: 'bubbles')])
+    Link.create(url: 'http://www.bubble-bobble.com',
+                title: 'Bubble Bobble',
+                tags: [Tag.first_or_create(name: 'bubbles')])
+  end
+
+
+
   scenario 'I can see existing links on the links page' do
     # create an existing link first.
     # we'll see where the create method comes from shortly
@@ -20,5 +37,13 @@ feature 'Viewing links' do
     within 'ul#links' do
       expect(page).to have_content('Makers Academy')
     end
+  end
+
+  scenario 'I can filter links by tag' do
+    visit '/tags/bubbles'
+    expect(page).not_to have_content('Makers Academy')
+    expect(page).not_to have_content('Code.org')
+    expect(page).to have_content('This is Zombocom')
+    expect(page).to have_content('Bubble Bobble')
   end
 end
