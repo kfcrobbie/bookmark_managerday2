4f80062d58d5a5d8cecfda3aac4e11c3739a6923
Robert Morgan <robbie.morgan@gmail.com>
2015-07-01 21:29:26 +0100

    refactored tests

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 feature 'User sign up' do
 
   # Strictly speaking, the tests that check the UI
@@ -8,33 +10,37 @@ feature 'User sign up' do
   # However, we are currently driving everything through
   # feature tests and we want to keep this example simple.
 
+  before(:each) { @user = User.new(user_attributes)}
 
   scenario 'I can sign up as a new user' do
-    expect { sign_up }.to change(User, :count).by(1)
+    expect { sign_up(@user) }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
 
   scenario 'With a password that does not match' do
-    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    @user.password_confirmation= 'wrong'
+    expect { sign_up(@user) }.not_to change(User, :count)
     expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
     expect(page).to have_content('Sorry, your passwords do not match')
   end
 
   scenario 'With an email that is already registered' do
-    sign_up_as(user)
-    expect { sign_up_as(user) }.to change(User, :count).by(0)
+    sign_up(@user)
+    expect { sign_up(@user) }.to change(User, :count).by(0)
     expect(page).to have_content('This email is already taken')
   end
 
-  def sign_up(email: 'alice@example.com',
-              password: '12345678',
-              password_confirmation: '12345678')
+  def sign_up(user)
     visit '/users/new'
     fill_in :email, with: user.email
     fill_in :password, with: user.password
     fill_in :password_confirmation, with: user.password_confirmation
     click_button 'Sign up'
+  end
+
+  def user_attributes
+    {email: 'alice@example.com', password: '12345678', password_confirmation: '12345678'}
   end
 
 end
