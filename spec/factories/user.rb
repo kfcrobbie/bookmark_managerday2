a680462d5feb134691bc2b7908aa7e452a9e6607
Robert Morgan <robbie.morgan@gmail.com>
2015-07-01 19:40:14 +0100

    New branch trying factory girl

---
+++ spec/factories/user.rb
@@ -0,0 +1,11 @@
+require './app/models/user.rb'
+
+FactoryGirl.define do
+
+  factory :user do
+    email 'alice@example.com'
+    password '12345678'
+    password_confirmation '12345678'
+  end
+
+end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 feature 'User sign up' do
 
   # Strictly speaking, the tests that check the UI
@@ -9,27 +11,30 @@ feature 'User sign up' do
   # feature tests and we want to keep this example simple.
 
 
-  scenario 'I can sign up as a new user' do
+  scenario 'I can sign up as a new user' do  
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
 
-  scenario 'With a password that does not match' do
-    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  scenario 'With an email that is already registered' do
+    sign_up
+    expect { sign_up }.to change(User, :count).by(0)
+    expect(page).to have_content('This email is already taken')
+  end
+
+   scenario 'With a password that does not match' do
+   
+    expect { sign_up }.not_to change(User, :count)
     expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
     expect(page).to have_content('Sorry, your passwords do not match')
   end
 
-  scenario 'With an email that is already registered' do
-    sign_up_as(user)
-    expect { sign_up_as(user) }.to change(User, :count).by(0)
-    expect(page).to have_content('This email is already taken')
-  end
-
-  def sign_up(email: 'alice@example.com',
-              password: '12345678',
-              password_confirmation: '12345678')
+  def sign_up
+    user = build :user 
+  # (email: 'alice@example.com',
+  #             password: '12345678',
+  #             password_confirmation: '12345678')
     visit '/users/new'
     fill_in :email, with: user.email
     fill_in :password, with: user.password
